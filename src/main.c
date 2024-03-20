0ed49532a2d5a509937509e72e7120bc934b7910
Agustin-Maffini <maffiniagustin@gmail.com>
2024-03-20 01:30:26 -0300

    Add and link libraries

--- src/main.c
+++ src/main.c
@@ -4,5 +4,7 @@
 int main(int argc, char* argv[]){
     printf("Hello, OS II. Welcome to 2024 course!\n");
     greetings();
+    printf("\nCalling shared lib from main program. I get '%d'\n", shared_lib_function("Hello OS II"));
+    printf("\nCalling static lib from main program. I get '%d'\n", static_lib_function("Bye OS II"));
     return 0;
 }
