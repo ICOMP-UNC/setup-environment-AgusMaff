8a20eda01c4bc5cd218c0aeb5d4a5b00d1879b38
Agustin-Maffini <maffiniagustin@gmail.com>
2024-03-20 00:40:56 -0300

    -Add executable

---
+++ src/greetings.c
@@ -0,0 +1,7 @@
+#include "greetings.h"
+#include <stdio.h>
+
+void greetings()
+{
+    printf("Hello fro another source file!\n");
+}
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <greetings.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    printf("Hello, OS II. Welcome to 2024 course!\n");
+    greetings();
+    return 0;
+}
